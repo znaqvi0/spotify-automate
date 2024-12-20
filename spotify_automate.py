eca059a630089109d93e9f99d572c41279dadb85
znaqvi0 <znaqvi2006@gmail.com>
2024-12-20 15:00:57 -0500

    refactored spotify management into SpotifyManager class

--- spotify_automate.py
+++ spotify_automate.py
@@ -3,74 +3,50 @@ import os
 import time
 
 import requests.exceptions
-import spotipy
 from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyOAuth
 
-from audio_processing import get_current_playback, is_ad_playing, song_time_left
+from audio_processing import SpotifyManager
 from system_management import SystemManager
-
-load_dotenv()
-
-client_id = os.getenv('SPOTIFY_CLIENT_ID')
-client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
-REDIRECT_URI = 'http://localhost:8000'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-def authorize(client_id, client_secret, redirect_uri):
-    auth_manager = SpotifyOAuth(client_id=client_id,
-                                client_secret=client_secret,
-                                redirect_uri=redirect_uri,
-                                scope='user-read-playback-state user-read-currently-playing')
-    return spotipy.Spotify(auth_manager=auth_manager)
+def print_time_left(spotify_manager):
+    time_ms = spotify_manager.get_time_left()
+    seconds_left = round(time_ms / 1000, 1) if time_ms is not None else 'N/A'
+    logging.info(f"{spotify_manager.get_current_playback()} ({seconds_left} seconds left)")
 
 
-def spotify_running_check(result, sys_manager):
-    if result:
-        return
-    if not sys_manager.is_spotify_running():
-        sys_manager.open_play()
-
-
-def print_time_left(result, time_left):
-    left = round(time_left / 1000, 1) if time_left is not None else 'N/A'
-    logging.info(f"{get_current_playback(result)} ({left} seconds left)")
-
-
-def time_check(result, sleep_time):
-    time_left = song_time_left(result)
-    print_time_left(result, time_left)
+def sleep(spotify_manager, sleep_time):
+    time_left = spotify_manager.get_time_left()
     if not time_left:
         return
     time.sleep(min(sleep_time, 0.7 * time_left / 1000))
 
 
-def advertisement_check(result, sys_manager):
-    if not is_ad_playing(result):
-        return False
+def main(sleep_interval):
+    load_dotenv()
+    client_id = os.getenv('SPOTIFY_CLIENT_ID')
+    client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+    redirect_uri = 'http://localhost:8000'
 
-    if result['is_playing']:
-        sys_manager.play_pause_media()
-    sys_manager.wait_until_spotify_closed()
-    sys_manager.open_play()
-    return True
+    spotify_manager = SpotifyManager(client_id, client_secret, redirect_uri)
+    system_manager = SystemManager()
 
-
-def main(sp, sys_manager, sleep_time):
-    sys_manager.prevent_sleep()
-    result = sp.current_playback()
-    time_check(result, sleep_time)
-    if not advertisement_check(result, sys_manager):
-        spotify_running_check(result, sys_manager)
-
-
-def run_main(client_id, client_secret, redirect_uri, sys_manager, sleep_time):
-    sp = authorize(client_id, client_secret, redirect_uri)
     while True:
-        main(sp, sys_manager, sleep_time)
+        spotify_manager.update_playback()
+        system_manager.prevent_sleep()
+
+        if not spotify_manager.current_playback:  # shortcut to checking if spotify is open
+            if not system_manager.is_spotify_running():
+                system_manager.open_play()
+
+        if spotify_manager.is_ad_playing():
+            spotify_manager.skip_ad(system_manager)
+
+        print_time_left(spotify_manager)
+
+        sleep(spotify_manager, sleep_interval)
 
 
 sleep_time = 5
@@ -79,7 +55,7 @@ sys_manager = SystemManager()
 # to recreate the .exe file use command pyinstaller --onefile spotify_automate.py
 def run_program():
     try:
-        run_main(client_id, client_secret, REDIRECT_URI, sys_manager, sleep_time)
+        main(sleep_time)
     except KeyboardInterrupt:
         logging.info("Program interrupted by user. Shutting down...")
         os.system(f'taskkill /pid {os.getpid()} /f')
